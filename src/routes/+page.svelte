4a43c5d2abafce75f7e481d95b647e6a30a5872e
siennajj <105862771+siennajj@users.noreply.github.com>
2023-05-16 10:42:12 +0200

    1 file modified, 3 added, 12 deleted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,30 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
 	h1 {
-		width: 100%;
+		font-size: 2em;
+		color: #333;
+		text-align: center;
+		margin-top: 2em;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	ul {
+		list-style: none;
+		padding: 0;
+		margin-top: 2em;
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	li {
+		font-size: 1.2em;
+		margin-bottom: 1em;
 	}
 </style>
+
+<main>
+<ul>
+	<h1>Data Visualiztion </h1>
+	<li> Name: Sienna Jeong student number : r0881089 University: KU Leuven </li> 
+</ul>
+</main>
+
